32fab388393bf2209a4ef8a9816bd1e11265ef46
werka-g <weronikagin@gmail.com>
2025-06-28 16:21:59 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://cbouspjmailotmgxqlpt.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNib3VzcGptYWlsb3RtZ3hxbHB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNTM5MjcsImV4cCI6MjA2MzkyOTkyN30.5a4Hv2iHxW-LvW91WffHyowJIezrVqv5g5mqBzs13r4';
-export const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://cbouspjmailotmgxqlpt.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNib3VzcGptYWlsb3RtZ3hxbHB0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgzNTM5MjcsImV4cCI6MjA2MzkyOTkyN30.5a4Hv2iHxW-LvW91WffHyowJIezrVqv5g5mqBzs13r4'
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 async function fetchArticles() {
   const { data, error } = await supabase
@@ -53,27 +50,6 @@ document.getElementById('articleForm').addEventListener('submit', async (e) => {
   } else {
     alert('Artykuł dodany!')
     e.target.reset()
-    fetchArticles() // Odśwież listę
+    fetchArticles()
   }
 })
-
-
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
